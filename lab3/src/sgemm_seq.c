d249e7739f70f187d0c0f440aee2f7970bceb297
LMBarboza <leon.uchoa@gmail.com>
2024-05-20 02:00:48 -0300

    add include and header files

---
+++ lab3/src/sgemm_seq.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/le_matriz.h"
+#include "include/timer.h"
+
+void sgemm(float *A, float *B, float *C) {}
+
+int main(int argc, char *argv[]) { return 0; }
